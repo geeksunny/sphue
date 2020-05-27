a3ae3c022544c95b362054ae6854039ce3518338
Justin Swanson <geeksunny@gmail.com>
2020-05-27 04:29:28 -0500

    Migrating pgm string & enum tools to external lib

--- include/prog_str.h
+++
@@ -1,9 +0,0 @@
-#ifndef SPHUE_INCLUDE_PROG_STR_H_
-#define SPHUE_INCLUDE_PROG_STR_H_
-
-#include <WString.h>
-
-String read_prog_str(const char *progmem_str);
-char *read_prog_str(const char *progmem_str, char *dest);
-
-#endif //SPHUE_INCLUDE_PROG_STR_H_
